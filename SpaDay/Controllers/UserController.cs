8385707354aef92e9bc81510782a39d5bf7f65ea
Rowan Polster <rowandownriver94@gmail.com>
2021-09-17 16:26:08 -0500

    completed chapter 14 studio plus bonuses

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.Username = newUser.Username;
+            ViewBag.Email = newUser.Email;
+            ViewBag.Password = newUser.Password;
+            ViewBag.verify = verify;
+            ViewBag.DateJoined = newUser.DateJoined;
+
+            if (newUser.Password == verify)
+            {
+                ViewBag.isError = false;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.isError = true;
+                ViewBag.Error = "Your passwords do not match! Try again!";
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+        public DateTime DateJoined { get; }
+
+        public User()
+        {
+            DateJoined = DateTime.Now;
+        }
+
+        public User(string username, string email, string password): this()
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
